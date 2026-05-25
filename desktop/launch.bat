e9b5b6f0cfc4d1e29c11a8fbf8cafe2e0a3c7f2a
Bazenga254 <bonitocheluget@gmail.com>
2026-05-25 07:52:46 +0000

    fix launch.bat: use git fetch+reset instead of pull to avoid merge conflicts

--- desktop/launch.bat
+++ desktop/launch.bat
@@ -1,7 +1,8 @@
 @echo off
 cd /d "%~dp0\.."
 echo Pulling latest code from GitHub...
-git pull origin main
+git fetch origin master
+git reset --hard origin/master
 cd /d "%~dp0"
 set ELECTRON_RUN_AS_NODE=
 node_modules\electron\dist\electron.exe .
